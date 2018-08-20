b0b8cee70b9719f8dacbc6d00da81da8bd5785e7
VinokurovAF <Vinokurovalexandr@mail.ru>
2018-08-20 12:26:29 +0400

    1

---
+++ contact-form1.php
@@ -0,0 +1,43 @@
+<?php
+
+/* Задаем переменные */
+$name = htmlspecialchars($_POST["name"]);
+
+$tel = htmlspecialchars($_POST["tel"]);
+
+
+$bezspama = htmlspecialchars($_POST["bezspama"]);
+
+/* Ваш адрес и тема сообщения */
+$address = "delaemsait@yandex.ry";
+$sub = "Сообщение с сайта DELAEMSAIT";
+
+/* Формат письма */
+$mes = "Сообщение с сайта(подвала).\n
+
+Имя отправителя: $name 
+Телефон отправителя: $tel
+
+Позвоните мне, у меня есть вопросы
+"
+
+;
+
+
+if (empty($bezspama)) /* Оценка поля bezspama - должно быть пустым*/
+{
+/* Отправляем сообщение, используя mail() функцию */
+$from = "Reply-To: $email \r\n";
+if (mail($address, $sub, $mes, $from)) {
+	header('Refresh: 5; URL=index.html');
+	echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Письмо отправлено, через 5 секунд вы вернетесь на страницу XXX</body>';}
+else {
+	header('Refresh: 5; URL=index.html');
+	echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Письмо не отправлено, через 5 секунд вы вернетесь на страницу YYY</body>';}
+}
+exit; /* Выход без сообщения, если поле bezspama чем-то заполнено */
+?>
